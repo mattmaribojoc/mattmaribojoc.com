d6c8ceab85fd0c451e10d4c4456046f3c533dcd0
Matt Maribojoc <matthewmaribojoc@gmail.com>
2023-03-19 17:23:34 -0400

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import storyblok from '@storyblok/astro'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 import vue from '@astrojs/vue'
 import tailwind from '@astrojs/tailwind' // https://astro.build/config
-import { loadEnv } from 'vite'
 
 import vercel from '@astrojs/vercel/serverless'
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
   integrations: [
     storyblok({
       accessToken: 'bRb5610UFzeXH0JFTQZQpwtt',
@@ -36,5 +34,6 @@ export default defineConfig({
       https: true
     }
   },
+  output: 'server',
   adapter: vercel()
 })
